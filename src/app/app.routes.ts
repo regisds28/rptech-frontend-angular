95f5f6a2f3c3a1b0c700d945aae52fd269397039
Régis Paiva Araújo <regisparaujo@gmail.com>
2024-03-15 17:11:54 -0300

    include first routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProjetosComponent } from './projetos/projetos/projetos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'projetos'
+  },
+  {
+    path: 'projetos',
+    loadChildren: () => import('./projetos/projetos.module').then(m => m.ProjetosModule)
+  }
+];
